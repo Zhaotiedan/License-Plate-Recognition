5ba677c2de1b7097c2d61935867be99fda14f828
Zhaotiedan <641691302@qq.com>
2021-03-26 17:42:54 +0800

    add the license plate recognition

---
+++ License Plate Recognition/lpr/include/Recognizer.h
@@ -0,0 +1,17 @@
+
+#ifndef SWIFTPR_RECOGNIZER_H
+#define SWIFTPR_RECOGNIZER_H
+
+#include "PlateInfo.h"
+#include "opencv2/dnn.hpp"
+namespace pr{
+    typedef cv::Mat label;
+    class GeneralRecognizer{
+        public:
+            virtual label recognizeCharacter(cv::Mat character) = 0;
+            void SegmentBasedSequenceRecognition(PlateInfo &plateinfo);
+
+    };
+
+}
+#endif //SWIFTPR_RECOGNIZER_H
