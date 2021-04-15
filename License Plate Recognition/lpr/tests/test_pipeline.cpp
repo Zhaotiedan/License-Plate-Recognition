0defd3bd0f213c9d0ab8e4335e6febf09aa0b991
Zhaotiedan <641691302@qq.com>
2021-04-15 20:52:29 +0800

    fix some bug

--- License Plate Recognition/lpr/tests/test_pipeline.cpp
+++ License Plate Recognition/lpr/tests/test_pipeline.cpp
@@ -1,16 +1,12 @@
 
-
+#pragma execution_character_set("utf-8")
 #include "../include/Pipeline.h"
 using namespace pr;
-
-
-//mine
-#include<string>
 using namespace std;
+#include<string>
+#include<vector>
 
 
-
-#if 0
 void TEST_PIPELINE(){
 
     pr::PipelinePR prc("../lpr/model/cascade.xml",
@@ -19,67 +15,36 @@ void TEST_PIPELINE(){
                       "../lpr/model/CharacterRecognization.prototxt","../lpr/model/CharacterRecognization.caffemodel"
                     );
 
-    cv::Mat image = cv::imread("../7.jpg");//选择测试图片
-    cv::imshow("image",image);
-    cv::waitKey(0);
-
-    std::vector<pr::PlateInfo> res = prc.RunPiplineAsImage(image);
-    float conf = 0 ;
-    for(auto st:res) {
-        if(st.confidence>0.1) {
-            std::cout << st.getPlateName() << " " << st.confidence << std::endl;
-            conf += st.confidence;
-        }
-    }
-    std::cout<<conf<<std::endl;
-	system("pause");
-
-
-}
-
-#endif
-
-void TEST_PIPELINE()
-{
 	string ImgName;
 	string ImgName1;
-
 	//测试集文件下的txt文件路径
-	ifstream fin("F:\\github\\HyperLPR-master-master\\HyperLPR-master-master\\Prj-Win\\检测车牌照片\\车牌文件名.txt");
-	//打开要输出的.csv文件
-	ofstream oFile;
-	oFile.open("检测车牌信息.csv", ios::out | ios::trunc);
-	//先写入表格标题
-	oFile << "车牌号" << "," << "测试文件名" << endl;
-	//判断是否读完txt
-	while (getline(fin, ImgName)) {
+	ifstream fin("F:\\github\\毕设\\License-Plate-Recognition\\License Plate Recognition\\检测车牌照片\\车牌文件名.txt");
+	while (getline(fin, ImgName))
+	{
 		cout << "Handle" << ImgName << endl;
-		ImgName1 = "F:\\github\\HyperLPR - master - master\\HyperLPR - master - master\\Prj - Win\\检测车牌照片" + ImgName;
-		//image = imread(ImgName); 
+		ImgName1 = "F:\\github\\毕设\\License-Plate-Recognition\\License Plate Recognition\\检测车牌照片" + ImgName;
 		cv::Mat image = cv::imread(ImgName1);
-		if (!image.empty()) {
-			//使用端到端模型模型进行识别，识别结果将会保存在res里面
-			std::vector<pr::PlateInfo> res = prc.RunPiplineAsImage(image);
-			float maxconf = 0.5; //初始最大置信度
-			string finaln;
-			for (auto st : res) {
-				//判断最大置信度下的识别结果		
-				if (st.confidence > maxconf) {
-					//std::cout << st.getPlateName() << " " << st.confidence << std::endl;
-					maxconf = st.confidence;
-					finaln = st.getPlateName();
 
-				}
+		//cv::Mat image = cv::imread("../7.jpg");//选择测试图片
+
+		cv::imshow("image", image);
+		cv::waitKey(0);
+
+		std::vector<pr::PlateInfo> res = prc.RunPiplineAsImage(image);
+		float conf = 0;
+		for (auto st : res) {
+			if (st.confidence > 0.1) {
+				std::cout << st.getPlateName() << " " << st.confidence << std::endl;
+				conf += st.confidence;
 			}
-			//将结果写入到.csv文件中
-			oFile << finaln << "," << ImgName1 << endl;
-			cout << "Done" << endl;
 		}
+		std::cout << conf << std::endl;
 	}
-	oFile.close();
-	cvWaitKey(0);
+	system("pause");
 
 }
+
+
 int main()
 {
 
