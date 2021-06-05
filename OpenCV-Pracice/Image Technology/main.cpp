e0a088b09bdab43881d1b3337979c863ebd69699
Zhaotiedan <48014011+Zhaotiedan@users.noreply.github.com>
2021-06-05 11:46:21 +0800

    create opencv-practice

---
+++ OpenCV-Pracice/Image Technology/main.cpp
@@ -0,0 +1,6 @@
+参考blog：
+1.高斯模糊：https://blog.csdn.net/sileixinhua/article/details/78232758
+2.灰度化处理：https://blog.csdn.net/sileixinhua/article/details/78232579
+3.sobel：
+  原理介绍：https://www.cnblogs.com/chenzhen0530/p/14660498.html
+  代码运用：https://blog.csdn.net/sileixinhua/article/details/78232791
